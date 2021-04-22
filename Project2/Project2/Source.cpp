471f57bca4efeee0ea1026dc31d080cf1b03c70c
Usec-chan <83037494+Usec-chan@users.noreply.github.com>
2021-04-23 00:19:48 +0400

    Add files via upload
    two version

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -2,6 +2,8 @@
 #include <SFML/Audio.hpp>
 #include "windows.h"
 #include "iostream"
+#include <string>
+#include <sstream>
 
 using namespace sf;
 
@@ -215,6 +217,14 @@ int main()
 	Texture tileSet;
 	tileSet.loadFromFile("C:\\Users\\kalex\\source\\repos\\Project2\\texture\\Mario_tileset.png");
 
+	Font font;
+	font.loadFromFile("C:\\Users\\kalex\\Desktop\\sansation.ttf");
+	Text mytext("Hello!", font, 50);
+	Text point("points: ", font, 50);
+	point.setPosition(10, 10);
+	mytext.setPosition(220, 10);
+	int q = 0;
+	q = points;
 
 	PLAYER Mario(tileSet);
 	ENEMY  enemy;
@@ -234,153 +244,152 @@ int main()
 	Clock clock;
 
 	int a = 1;
-	
-	void game();
+
+
+	while (window.isOpen())
 	{
-		while (window.isOpen())
+		double time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+
+		time = time / 500;  // здесь регулируем скорость игры
+
+		if (time > 20)
 		{
-			double time = clock.getElapsedTime().asMicroseconds();
-			clock.restart();
-
-			time = time / 500;  // здесь регулируем скорость игры
-
-			if (time > 20)
-			{
-				time = 20;
-			}
-
-			Event event;
-			while (window.pollEvent(event))
-			{
-				if (event.type == Event::Closed)
-				{
-					window.close();
-				}
-			}
-
-
-			if (Keyboard::isKeyPressed(Keyboard::Left))
-			{
-				Mario.dx = -0.1;
-			}
-
-			if (Keyboard::isKeyPressed(Keyboard::Right))
-			{
-				Mario.dx = 0.1;
-			}
-
-			if (Keyboard::isKeyPressed(Keyboard::Up))
-			{
-				if (Mario.onGround)
-				{
-					Mario.dy = -0.27;
-					Mario.onGround = false;
-					sound.play();
-				}
-			}
-
-
-			Mario.update(time);
-			enemy.update(time);
-
-
-			if (Mario.rect.intersects(enemy.rect))
-			{
-				if (enemy.life)
-				{
-					if (Mario.dy > 0)
-					{
-						enemy.dx = 0;
-						Mario.dy = -0.2;
-						enemy.life = false;
-					}
-					else
-					{
-						game;
-						//Mario.sprite.setColor(Color::Red);
-					}
-				}
-			}
-
-
-			if (Mario.rect.left > 200)
-			{
-				offsetX = Mario.rect.left - 200;           //смещение
-			}
-
-
-			window.clear(Color(107, 140, 255));
-
-			for (int i = 0; i < H; i++)
-			{
-				for (int j = 0; j < W; j++)
-				{
-					if (TileMap[i][j] == 'P')
-					{
-						tile.setTextureRect(IntRect(143 - 16 * 3, 112, 16, 16));
-					}
-
-					if (TileMap[i][j] == 'k')
-					{
-						tile.setTextureRect(IntRect(143, 112, 16, 16));
-					}
-
-					if (TileMap[i][j] == 'c')
-					{
-						tile.setTextureRect(IntRect(143 - 16, 112, 16, 16));
-					}
-
-					if (TileMap[i][j] == 't')
-					{
-						tile.setTextureRect(IntRect(0, 47, 32, 95 - 47));
-					}
-
-					if (TileMap[i][j] == 'g')
-					{
-						tile.setTextureRect(IntRect(0, 16 * 9 - 5, 3 * 16, 16 * 2 + 5));
-					}
-
-					if (TileMap[i][j] == 'G')
-					{
-						tile.setTextureRect(IntRect(145, 222, 222 - 145, 255 - 222));
-					}
-
-					if (TileMap[i][j] == 'd')
-					{
-						tile.setTextureRect(IntRect(0, 106, 74, 127 - 106));
-					}
-
-					if (TileMap[i][j] == 'w')
-					{
-						tile.setTextureRect(IntRect(99, 224, 140 - 99, 255 - 224));
-					}
-
-					if (TileMap[i][j] == 'r')
-					{
-						tile.setTextureRect(IntRect(143 - 32, 112, 16, 16));
-					}
-
-					if ((TileMap[i][j] == ' ') || (TileMap[i][j] == '0'))
-					{
-						continue;
-					}
-
-					tile.setPosition(j * 16 - offsetX, i * 16 - offsetY);
-					window.draw(tile);
-				}
-			}
-
-			window.draw(Mario.sprite);
-			window.draw(enemy.sprite);
-
-			window.display();
+			time = 20;
 		}
-		switch (a)
+
+		Event event;
+		while (window.pollEvent(event))
 		{
-		case 1:
-			game;
-		case 2:
-			a = 1;
+			if (event.type == Event::Closed)
+			{
+				window.close();
+			}
 		}
+
+
+		if (Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			Mario.dx = -0.1;
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			Mario.dx = 0.1;
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+		{
+			if (Mario.onGround)
+			{
+				Mario.dy = -0.27;
+				Mario.onGround = false;
+				sound.play();
+			}
+		}
+
+
+		Mario.update(time);
+		enemy.update(time);
+
+
+		if (Mario.rect.intersects(enemy.rect))
+		{
+			if (enemy.life)
+			{
+				if (Mario.dy > 0)
+				{
+					enemy.dx = 0;
+					Mario.dy = -0.2;
+					enemy.life = false;
+				}
+				else
+				{
+					Mario.sprite.setColor(Color::Red);
+				}
+			}
+		}
+
+
+		if (Mario.rect.left > 200)
+		{
+			offsetX = Mario.rect.left - 200;           //смещение
+		}
+
+
+		window.clear(Color(107, 140, 255));
+
+		for (int i = 0; i < H; i++)
+		{
+			for (int j = 0; j < W; j++)
+			{
+				if (TileMap[i][j] == 'P')
+				{
+					tile.setTextureRect(IntRect(143 - 16 * 3, 112, 16, 16));
+				}
+
+				if (TileMap[i][j] == 'k')
+				{
+					tile.setTextureRect(IntRect(143, 112, 16, 16));
+				}
+
+				if (TileMap[i][j] == 'c')
+				{
+					tile.setTextureRect(IntRect(143 - 16, 112, 16, 16));
+				}
+
+				if (TileMap[i][j] == 't')
+				{
+					tile.setTextureRect(IntRect(0, 47, 32, 95 - 47));
+				}
+
+				if (TileMap[i][j] == 'g')
+				{
+					tile.setTextureRect(IntRect(0, 16 * 9 - 5, 3 * 16, 16 * 2 + 5));
+				}
+
+				if (TileMap[i][j] == 'G')
+				{
+					tile.setTextureRect(IntRect(145, 222, 222 - 145, 255 - 222));
+				}
+
+				if (TileMap[i][j] == 'd')
+				{
+					tile.setTextureRect(IntRect(0, 106, 74, 127 - 106));
+				}
+
+				if (TileMap[i][j] == 'w')
+				{
+					tile.setTextureRect(IntRect(99, 224, 140 - 99, 255 - 224));
+				}
+
+				if (TileMap[i][j] == 'r')
+				{
+					tile.setTextureRect(IntRect(143 - 32, 112, 16, 16));
+				}
+
+				if ((TileMap[i][j] == ' ') || (TileMap[i][j] == '0'))
+				{
+					continue;
+				}
+
+				tile.setPosition(j * 16 - offsetX, i * 16 - offsetY);
+				window.draw(tile);
+			}
+		}
+
+		mytext.setString(to_string(q));
+
+		window.draw(Mario.sprite);
+		window.draw(enemy.sprite);
+
+		window.display();
 	}
+	/*switch (a)
+	{
+	case 1:
+		game;
+	case 2:
+		a = 1;
+	*/
 }
